67759541c5c5211c26bb722905f1cd5386724663
tekofx <tekofxx@gmail.com>
2025-04-05 12:56:46 +0200

    Added cron

--- backend/src/database.ts
+++ backend/src/database.ts
@@ -1,5 +1,10 @@
 import mongoose from "mongoose";
 import config from "./config";
+import axios from "axios";
+import TrainData from "./interfaces/trainData";
+import trainModel from "./trainModel";
+
+const fgcApiUrl = "https://dadesobertes.fgc.cat/api/explore/v2.1/catalog/datasets/posicionament-dels-trens/records?where=lin%20IN%20(%22R5%22%2C%20%22R6%22%2C%20%22S4%22%2C%20%22S8%22)&limit=20"
 
 const connectDB = async () => {
     try {
@@ -11,4 +16,43 @@ const connectDB = async () => {
     }
 };
 
-export default connectDB;
+const fetchData = async () => {
+    try {
+        const response = await axios.get(fgcApiUrl);
+        const results: Array<TrainData> = response.data.results;
+
+        // Filter out records older than 30 minutes
+        const thirtyMinutesAgo = new Date(Date.now() - 30 * 60 * 1000);
+
+        for (let i = 0; i < results.length; i++) {
+            const result = results[i];
+            const nextStops = result.properes_parades.split(';').map(item => JSON.parse(item));
+
+            const existingRecord = await trainModel.findOne({
+                line: result.lin,
+                origin: result.origen,
+                destination: result.desti,
+                nextStops: nextStops,
+                time: {$gte: thirtyMinutesAgo}
+            });
+
+            if (existingRecord != null) {
+                console.log(`Found record: ${existingRecord}`);
+                await trainModel.create({
+                    id: result.id,
+                    line: result.lin,
+                    origin: result.origen,
+                    destination: result.desti,
+                    nextStops: nextStops,
+                    time: new Date(),
+                    occupation: result.ocupacio_mi_percent / 100
+                });
+            }
+        }
+        console.log("Finished data");
+    } catch (error) {
+        console.error('Error fetching data:', error);
+    }
+};
+
+export {connectDB, fetchData};
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,22 +1,20 @@
 import express from "express";
-import axios from "axios";
-import connectDB from "./database";
+import {connectDB, fetchData} from "./database";
 import trainModel from "./trainModel";
-import TrainData from "./interfaces/trainData";
 import cors from "cors";
 import * as path from "node:path";
-
-const fgcApiUrl = "https://dadesobertes.fgc.cat/api/explore/v2.1/catalog/datasets/posicionament-dels-trens/records?where=lin%20IN%20(%22R5%22%2C%20%22R6%22%2C%20%22S4%22%2C%20%22S8%22)&limit=20"
+import cron from "node-cron";
 
 const app = express();
 const port = 1234;
 
+
+// Middleware
 app.use(cors())
-
-connectDB()
-
+app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
 
+connectDB();
 
 app.get('/data', async (req, res) => {
     await trainModel.find({}).then((data) => {
@@ -31,56 +29,17 @@ app.get("/fetch", async (req, res) => {
     })
 })
 
+
 // Serve the index.html file in production
 app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 
-app.use(express.json());
-
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
+    cron.schedule("*/5 * * * *", async () => {
+        await fetchData();
+    })
 })
 
-const fetchData = async () => {
-    try {
-        const response = await axios.get(fgcApiUrl);
-        const results: Array<TrainData> = response.data.results;
-
-        // Filter out records older than 30 minutes
-        const thirtyMinutesAgo = new Date(Date.now() - 30 * 60 * 1000);
-
-        for (let i = 0; i < results.length; i++) {
-            const result = results[i];
-            const nextStops = result.properes_parades.split(';').map(item => JSON.parse(item));
-
-            const existingRecord = await trainModel.findOne({
-                line: result.lin,
-                origin: result.origen,
-                destination: result.desti,
-                nextStops: nextStops,
-                time: {$gte: thirtyMinutesAgo}
-            });
-
-            if (!existingRecord) {
-                await trainModel.create({
-                    id: result.id,
-                    line: result.lin,
-                    origin: result.origen,
-                    destination: result.desti,
-                    nextStops: nextStops,
-                    time: new Date(),
-                    occupation: result.ocupacio_mi_percent / 100
-                });
-            }
-        }
-    } catch (error) {
-        console.error('Error fetching data:', error);
-    }
-};
-
-fetchData()
-
-// Set interval to run the fetchData function every 5 minutes (300000 milliseconds)
-setInterval(fetchData, 300000);
