91e8770a1aff146f176e7d1404253ebbd748761e
tekofx <tekofxx@gmail.com>
2025-04-08 18:38:16 +0200

    Added nextStop query argument

--- backend/src/routers.ts
+++ backend/src/routers.ts
@@ -9,11 +9,23 @@ const router = express.Router();
 router.get("/time", async (req, res) => {
 
     const line = req.query.line;
+    const nextStop = req.query.nextStop;
 
     const pipeline: any[] = [];
     if (line) {
         pipeline.push({$match: {line: line}});
     }
+
+    if (nextStop) {
+        pipeline.push({
+            $match: {
+                $expr: {
+                    $eq: [{$arrayElemAt: ["$nextStops", 0]}, nextStop]
+                }
+            }
+        });
+    }
+
 
     pipeline.push(
         {
