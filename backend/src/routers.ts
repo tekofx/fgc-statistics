ab0c92d769bdb15ffaa384d43599d491cdd5bc97
tekofx <tekofxx@gmail.com>
2025-04-13 11:33:30 +0200

    Added limit to 100

--- backend/src/routers.ts
+++ backend/src/routers.ts
@@ -6,58 +6,107 @@ import {fetchData} from "./database";
 const router = express.Router();
 
 
-router.get("/occupation", async (req, res) => {
-
-    const line = req.query.line;
-    const nextStop = req.query.nextStop;
+router.get('/data', async (req, res) => {
+    const filter = req.query.filter as string | undefined;
+    const sort = req.query.sort as string | undefined;
 
     const pipeline: any[] = [];
-    if (line) {
-        pipeline.push({$match: {line: line}});
-    }
 
-    if (nextStop) {
+    if (filter) {
+        const key = filter.split("=")[0];
+        const value = filter.split("=")[1];
         pipeline.push({
             $match: {
-                $expr: {
-                    $eq: [{$arrayElemAt: ["$nextStops", 0]}, nextStop]
+                [key]: {
+                    $regex: value,
+                    $options: "i"
                 }
             }
         });
     }
 
+    if (sort) {
+        const sortKey = sort.split(":")[0];
+        const sortDirection = parseInt(sort.split(":")[1]);
+        pipeline.push({$sort: {[sortKey]: sortDirection}});
+    }
 
-    pipeline.push(
+    pipeline.push({$limit: 100}); // Add a limit of 100 rows
+
+    const filtersPipeline = [
         {
-            $group: {
-                _id: {
-                    hour: {$hour: "$time"}
-                },
-                averageOccupation: {$avg: "$occupation"}
+            $facet: {
+                data: pipeline.length > 0 ? pipeline : [{$match: {}}],
+                filters: [
+                    {
+                        $group: {
+                            _id: null,
+                            line: {$addToSet: "$line"},
+                            origin: {$addToSet: "$origin"},
+                            destination: {$addToSet: "$destination"},
+                        }
+                    },
+                    {
+                        $project: {
+                            _id: 0,
+                            filters: {
+                                line: "$line",
+                                origin: "$origin",
+                                destination: "$destination",
+                            }
+                        }
+                    }
+                ]
             }
-        },
-        {
-            $sort: {"_id.hour": 1}
         },
         {
             $project: {
-                _id: 0,
-                hour: "$_id.hour",
-                averageOccupation: {$round: [{$multiply: ["$averageOccupation", 100]}, 2]}
+                data: {
+                    $map: {
+                        input: "$data",
+                        as: "item",
+                        in: {
+                            id: "$$item._id",
+                            time: {
+                                $dateToString: {
+                                    format: "%d/%m/%Y %H:%M",
+                                    date: "$$item.time",
+                                    timezone: "UTC"
+                                }
+                            },
+                            line: "$$item.line",
+                            origin: "$$item.origin",
+                            destination: "$$item.destination",
+                            occupation: "$$item.occupation",
+                            nextStops: {
+                                $reduce: {
+                                    input: "$$item.nextStops",
+                                    initialValue: "",
+                                    in: {
+                                        $concat: [
+                                            "$$value",
+                                            {$cond: [{$eq: ["$$value", ""]}, "", ", "]},
+                                            "$$this"
+                                        ]
+                                    }
+                                }
+                            }
+                        }
+                    }
+                },
+                filters: {$arrayElemAt: ["$filters.filters", 0]}
             }
         }
-    );
+    ];
 
-
-    await trainModel.aggregate(pipeline).then((data) => {
-        res.json(data);
+    await trainModel.aggregate(filtersPipeline).then((result) => {
+        const response = result[0];
+        res.json(response);
     }).catch((err) => {
         console.log(err);
         res.status(500).json({error: "Error fetching data"});
-    })
-
-})
-
+    });
+});
 router.get('/data', async (req, res) => {
     const filter = req.query.filter as string | undefined;
     const sort = req.query.sort as string | undefined;
